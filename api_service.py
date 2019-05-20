776500e72a022030de3b62fbf0920318f8e8d742
rainchik <rainchik.aa@gmail.com>
2019-05-20 23:41:39 +0300

    full version with commented json output on get request

--- api_service.py
+++ api_service.py
@@ -2,8 +2,6 @@ from flask import Flask
 from flask_restful import Api, Resource, reqparse
 import datetime
 import psycopg2
-import json
-from bson import json_util
 
 app = Flask(__name__)
 api = Api(app)
@@ -24,32 +22,54 @@ def date_converter(o):
     if isinstance(o, datetime.date):
         return o.strftime('%Y-%m-%d')
 
+def output_message(output_message):
+    output_json = {
+        "message": output_message
+    }
+    return output_json
+
+
 class User(Resource):
     def get(self, name):
         if not name.isalpha():
-            return "Name contains not only letters", 404
+            return output_message("Name contains not only letters"), 404
+        try:
+            cursor.execute("select * from users where name = (%s)", [name])
+            record = cursor.fetchone()
+        except:
+            return output_message("Something goes wrong with database"), 500
+        if record is None:
+            return output_message("User not found"), 404
 
-        cursor.execute("select * from users where name = (%s)", [name])
-        record = cursor.fetchone()
-        record_list = list(record)
-        colnames = [desc[0] for desc in cursor.description]
+        # print(record)
+        # record_list = list(record)
+        # colnames = [desc[0] for desc in cursor.description]
+        #
+        # user_json = {}
+        # for i in range(len(colnames)):
+        #     if isinstance(record_list[i], datetime.date):
+        #         record_list[i] = record_list[i].strftime('%Y-%m-%d')
+        #
+        #     user_json[colnames[i]] = record_list[i]
+        #
+        # print((record[1]))
+        # now = str_to_date(datetime.datetime.now().strftime("%Y-%m-%d"))
+        # print((now))
 
-        user_json = {}
-        for i in range(len(colnames)):
-            if isinstance(record_list[i], datetime.date):
-                record_list[i] = record_list[i].strftime('%Y-%m-%d')
+        today_date = datetime.date.today()
+        users_date = record[1]
 
-            user_json[colnames[i]] = record_list[i]
+        next_birthday = datetime.date(today_date.year, users_date.month, users_date.day)
+        if (next_birthday - today_date).days < 0:
+            next_birthday = datetime.date(today_date.year + 1, users_date.month, users_date.day)
 
+        if (next_birthday - today_date).days > 0:
+            message = ('Hello, ' + record[0] + '! Your birthday is in ' + str((next_birthday - today_date).days) + ' day(s)')
+        else:
+            message = ('Hello, ' + record[0] + '! Happy birthday!')
 
-        print((record[1]))
-        now = str_to_date(datetime.datetime.now().strftime("%Y-%m-%d"))
-        print((now))
+        return output_message(message), 200
 
-        return user_json, 200
-
-
-        # return "User not found", 404
 
     def put(self, name):
         parser = reqparse.RequestParser()
@@ -60,14 +80,16 @@ class User(Resource):
         now = str_to_date(datetime.datetime.now().strftime("%Y-%m-%d"))
 
         if date_of_birth >= now:
-            return "Date not valid. Should be less than today", 404
-        user = {
-            "name": name,
-            "dateOfBirth": args["dateOfBirth"]
-        }
+            return output_message("Date not valid. Should be less than today"), 404
 
-        users.append(user)
-        return user, 201
+
+        try:
+            cursor.execute("insert into users (name, dob) values ((%s), (%s))", [name, args["dateOfBirth"]])
+            connection.commit()
+        except:
+            return output_message("Something goes wrong with database"), 500
+
+        return '', 201
 
 
 try:
@@ -81,19 +103,15 @@ try:
     record = cursor.fetchone()
     print("You are connected to - ", record,"\n")
 
-
+    api.add_resource(User, "/hello/<string:name>")
+    app.run(debug=False)
 
 except (Exception, psycopg2.Error) as error :
     print ("Error while connecting to PostgreSQL", error)
     exit()
 
-
-api.add_resource(User, "/hello/<string:name>")
-app.run(debug=False)
-
-# finally:
-#     #closing database connection.
-if(connection):
-    cursor.close()
-    connection.close()
-    print("PostgreSQL connection is closed")
+finally:
+    if(connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")

5d4b3e55b8fbb0f350ebbb84c55ca9f2acfa28dd
rainchik <rainchik.aa@gmail.com>
2019-05-20 23:55:43 +0300

    pep8

--- api_service.py
+++ api_service.py
@@ -6,20 +6,23 @@ import psycopg2
 app = Flask(__name__)
 api = Api(app)
 
+
 def str_to_date(string_date):
     output = datetime.datetime.strptime(string_date, '%Y-%m-%d')
-    return(output)
+    return output
 
 
 def date_converter(o):
     if isinstance(o, datetime.date):
         return o.strftime('%Y-%m-%d')
 
-def output_message(output_message):
+
+def output_message(message):
     output_json = {
-        "message": output_message
+        "message": message
     }
     return output_json
+
 
 class User(Resource):
     def get(self, name):
@@ -41,9 +44,15 @@ class User(Resource):
             next_birthday = datetime.date(today_date.year + 1, users_date.month, users_date.day)
 
         if (next_birthday - today_date).days > 0:
-            message = ('Hello, ' + record[0] + '! Your birthday is in ' + str((next_birthday - today_date).days) + ' day(s)')
+            message = ('Hello, '
+                       + record[0]
+                       + '! Your birthday is in '
+                       + str((next_birthday - today_date).days)
+                       + ' day(s)')
         else:
-            message = ('Hello, ' + record[0] + '! Happy birthday!')
+            message = ('Hello, '
+                       + record[0]
+                       + '! Happy birthday!')
 
         return output_message(message), 200
 
@@ -61,7 +70,6 @@ class User(Resource):
         if date_of_birth >= now:
             return output_message("Date not valid. Should be less than today"), 404
 
-
         try:
             cursor.execute("insert into users (name, dob) values ((%s), (%s))", [name, args["dateOfBirth"]])
             connection.commit()
@@ -72,25 +80,25 @@ class User(Resource):
 
 
 try:
-    connection = psycopg2.connect(user = "postgres",
-                                  password = "",
-                                  host = "127.0.0.1",
-                                  port = "5432",
-                                  database = "api_db")
+    connection = psycopg2.connect(user="postgres",
+                                  password="",
+                                  host="127.0.0.1",
+                                  port="5432",
+                                  database="api_db")
     cursor = connection.cursor()
     cursor.execute("SELECT version();")
     record = cursor.fetchone()
-    print("You are connected to - ", record,"\n")
+    print("You are connected to - ", record, "\n")
 
     api.add_resource(User, "/hello/<string:name>")
     app.run(debug=False)
 
-except (Exception, psycopg2.Error) as error :
-    print ("Error while connecting to PostgreSQL", error)
+except (Exception, psycopg2.Error) as error:
+    print("Error while connecting to PostgreSQL", error)
     exit()
 
 finally:
-    if(connection):
+    if connection:
         cursor.close()
         connection.close()
         print("PostgreSQL connection is closed")
